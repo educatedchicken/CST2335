276a9b1c3a385e7949350b5b960f29585c0d560c
Marco <marco.quintanar79@gmail.com>
2018-10-05 14:00:12 -0400

    lab3

--- app/src/main/java/com/example/marco/andriodlabs/StartActivity.java
+++ app/src/main/java/com/example/marco/andriodlabs/StartActivity.java
@@ -1,13 +1,76 @@
 package com.example.marco.andriodlabs;
 
 import android.app.Activity;
+import android.app.ListActivity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class StartActivity extends Activity {
+
+    protected static final String ACTIVITY_NAME = "StartActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        Log.i(ACTIVITY_NAME, "In onCreate()");
+
+        Button button2 = (Button)findViewById(R.id.button2);
+
+        final  Intent intent = new Intent(this, ListItemsActivity.class);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivityForResult(intent,50);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int responseCode, Intent data) {
+        super.onActivityResult(requestCode, responseCode, data);
+        if (requestCode == 50) {
+            Log.i(ACTIVITY_NAME, "Returned to StartActivity.onActivityResult");
+
+            if (responseCode == Activity.RESULT_OK) {
+                String messagePassed = data.getStringExtra("Response");
+                Toast myToast = Toast.makeText(this, messagePassed, Toast.LENGTH_SHORT);
+            }
+        }
+    }
+
+    protected void onResume(){
+        super.onResume();
+        Log.i(ACTIVITY_NAME, "In onResume()");
+    }
+
+    protected void onStart(){
+        super.onStart();
+        Log.i(ACTIVITY_NAME, "In onStart()");
+    }
+
+    protected void onPause(){
+        super.onPause();
+        Log.i(ACTIVITY_NAME, "In onPause()");
+    }
+
+    protected void onStop(){
+        super.onStop();
+        Log.i(ACTIVITY_NAME, "In onStop()");
+    }
+
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.i(ACTIVITY_NAME, "In onDestroy()");
     }
 }
